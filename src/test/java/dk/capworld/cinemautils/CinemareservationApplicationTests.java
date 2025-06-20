3d2c612633cb7ee28c4ec2d35f8ba82f69d48146
Carsten Pedersen <carsten.pedersen@capworld.dk>
2025-06-21 01:28:41 +0200

    7. commit

--- src/test/java/dk/capworld/cinemautils/CinemareservationApplicationTests.java
+++ src/test/java/dk/capworld/cinemautils/CinemareservationApplicationTests.java
@@ -1,6 +1,7 @@
 package dk.capworld.cinemautils;
 
 import dk.capworld.cinemautils.domain.Shows;
+import dk.capworld.cinemautils.exceptions.BookingException;
 import dk.capworld.cinemautils.model.BookingRequest;
 import dk.capworld.cinemautils.model.BookingResult;
 import dk.capworld.cinemautils.repository.ShowsRepository;
@@ -74,6 +75,58 @@ class CinemareservationApplicationTests {
         if (bookingService.isAlreadyBooked(show.get(), 3)) {
             fail("Seat 3 at show 3 is not cancelled correctly.");
         }
+
+    }
+
+
+    /**
+     * Precondition is that show with id 5 must exist in table shows
+     */
+    @Test
+    void testBookingServiceMakeReservationAlreadyBooked() {
+        Optional<Shows> show = showsRepository.findById(5L);
+        if (show.isEmpty()) {
+            fail("Show with 5 is not set up as precondition.");
+        }
+
+        if (bookingService.isAlreadyBooked(show.get(), 1)) {
+            bookingService.cancelReservation(show.get(), 1);
+        }
+        if (bookingService.isAlreadyBooked(show.get(), 2)) {
+            bookingService.cancelReservation(show.get(), 2);
+        }
+        if (bookingService.isAlreadyBooked(show.get(), 3)) {
+            bookingService.cancelReservation(show.get(), 3);
+        }
+
+        List<Integer> reserveSeats = new ArrayList<>(Arrays.asList(1,2,3));
+        BookingRequest bookingRequest = BookingRequest.builder()
+                .showId(5L)
+                .seats(reserveSeats)
+                .build();
+        bookingService.makeReservations(bookingRequest);
+
+        List<Integer> reserveSeatsAlredyDone = new ArrayList<>(Arrays.asList(3));
+        BookingRequest bookingRequestAlreadyDone = BookingRequest.builder()
+                .showId(5L)
+                .seats(reserveSeatsAlredyDone)
+                .build();
+        try {
+            bookingService.makeReservations(bookingRequestAlreadyDone);
+        } catch (BookingException ex) {
+            assertEquals(BookingException.ErrorCode.ObjectCannotBeSaved, ex.getErrorCode());
+        }
+
+        List<BookingResult> bookingResults = bookingService.findAllShowsAndAvailableSeats();
+        List<BookingResult> filteredBookingResults = bookingResults.stream()
+                .filter(b -> b.getId().equals(5L))
+                .toList();
+
+        assertEquals(1, filteredBookingResults.size());
+        assertEquals(SEATS_IN_CINEMA-3, filteredBookingResults.getFirst().getAvailableSeats().size());
+
+        bookingService.cancelReservations(bookingRequest);
+
 
     }
 
